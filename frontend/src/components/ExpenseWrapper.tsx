0fa2597778106a95dc849d9473be7f50a6af415f
Enzo Panem <epanem@my.yorku.ca>
2026-01-02 14:52:24 -0500

    feature: implemented http client GET request and backend api get endpoint that handles the request. Sucessfully returns a float response to the client

--- frontend/src/components/ExpenseWrapper.tsx
+++ frontend/src/components/ExpenseWrapper.tsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import { ExpenseForm } from "./ExpenseForm";
 import type { Expense } from "../types";
 // import { v4 as uuidv4 } from "uuid";
-import { getAllExpenses } from "../services/api";
+import { fetchExpenseTotal, getAllExpenses } from "../services/api";
 import { ExpenseTable } from "./ExpenseTable";
 import { ExpenseReport } from "./ExpenseReport";
 
 export const ExpenseWrapper = () => {
   const [expenses, setExpenses] = useState<Expense[]>([]);
+  const [total, setTotal] = useState<number>(0);
 
   useEffect(() => {
     const fetchExpenses = async () => {
@@ -19,7 +20,18 @@ export const ExpenseWrapper = () => {
       }
     };
 
+    const fetchTotal = async () => {
+      try {
+        const data = await fetchExpenseTotal();
+        setTotal(data);
+      } catch (error) {
+        console.error("Failed to fetch total:", error);
+      }
+    }
+
     fetchExpenses();
+    fetchTotal();
+    
   }, []);
 
   const addExpense = (expense: Expense) => {
@@ -31,7 +43,7 @@ export const ExpenseWrapper = () => {
       <div className="flex flex-col items-center gap-4">
         <div className="flex gap-4">
           <ExpenseForm addExpense={addExpense} />
-          <ExpenseReport/>
+          <ExpenseReport expenseTotal={total}/>
         </div>
         <ExpenseTable expenses={expenses} />
       </div>
